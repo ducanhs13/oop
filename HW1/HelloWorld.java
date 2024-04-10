b56ca4a69608f0c6b86c225b9bde186aa708ca17
ducanhs13 <hoangducbang7@gmail.com>
2024-04-10 10:21:09 +0700

    em la bup mang non em lon len trong mua cach mang

--- HW1/HelloWorld.java
+++ HW1/HelloWorld.java
@@ -1,9 +1,11 @@
-public class HelloWorld {
+public class Main {
 
     public static void main(String[] args) {
-
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Trinh Hoang Duc '\t' 23020359 '\t' K68AI1 '\t' ducanhs13 '\t' hoangducbang7@gmail.com");
+        for (int i=9; i>0; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer." + "\n" + "Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
     }
 
 }
